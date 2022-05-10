685a390671519e79250d73f04f324e597eff8422
Dennis Ojeda <royojeda1@gmail.com>
2022-05-10 17:02:09 +0800

    Add a guesses array to store past guesses

--- game.rb
+++ game.rb
@@ -1,10 +1,13 @@
 class Game
   attr_reader :tracker
-  attr_accessor :guess, :remaining_guesses
+  attr_accessor :guesses, :remaining_guesses
+
+  GUESS_LIMIT = 6
 
   def initialize
+    @guesses = []
     system 'clear'
-    @remaining_guesses = 6
+    @remaining_guesses = GUESS_LIMIT
     show_remaining_guesses
     choose_word
     @tracker = Array.new(secret_word.length, '_')
@@ -13,17 +16,18 @@ class Game
 
   def play
     until remaining_guesses.zero?
-      take_guess
+      guesses[GUESS_LIMIT - remaining_guesses] = take_guess
       self.remaining_guesses -= 1
       system 'clear'
       show_remaining_guesses
       show_tracker
+      p guesses
     end
   end
 
   def take_guess
     puts 'Enter a guess: '
-    self.guess = gets.chomp.downcase
+    gets.chomp.downcase
   end
 
   def show_remaining_guesses
