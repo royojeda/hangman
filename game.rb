15a1a26bda9de997358890bab4bbe3133125dd56
Dennis Ojeda <royojeda1@gmail.com>
2022-05-10 16:25:57 +0800

    Add a method to display the 'tracker'
    The tracker initially consists of dashes for each letter of the secret
    word.

--- game.rb
+++ game.rb
@@ -1,11 +1,13 @@
 class Game
-  attr_reader :remaining_guesses
+  attr_reader :remaining_guesses, :tracker
 
   def initialize
     system 'clear'
     @remaining_guesses = 6
     show_remaining_guesses
     choose_word
+    @tracker = Array.new(secret_word.length, '_')
+    show_tracker
   end
 
   def show_remaining_guesses
@@ -21,6 +23,14 @@ class Game
 
     @secret_word = words[rand(0..(words.length - 1))]
   end
+
+  def show_tracker
+    puts tracker.join(' ')
+  end
+
+  private
+
+  attr_reader :secret_word
 end
 
 Game.new
