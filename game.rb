1691a7fe73551ffd24bb677fc8781cbbe4a209d4
Dennis Ojeda <royojeda1@gmail.com>
2022-05-10 15:36:36 +0800

    Add a method to read a dictionary file and randomly select a word

--- game.rb
+++ game.rb
@@ -1,7 +1,22 @@
 class Game
-  def initialize(word)
-    @word = word
+  attr_accessor :counter
+
+  def initialize
     @guesses_left = 6
+    @counter = 0
+    load_dictionary
+  end
+
+  def load_dictionary
+    words = File.open('google-10000-english-no-swears.txt', 'r') do |dictionary|
+      dictionary.readlines.filter_map do |word|
+        word.chomp if word.length.between?(6, 13)
+      end
+    end
+
+    secret_word = words[rand(0..(words.length - 1))]
+
+    p secret_word
   end
 
   private
@@ -9,3 +24,5 @@ class Game
   attr_reader :word
   attr_writer :guesses_left
 end
+
+Game.new
